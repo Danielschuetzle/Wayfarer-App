c2121f54dee40a573811bdc088ad029b49b82e0f
Daniel Schützle <danielschuetzle@gmail.com>
2023-06-20 16:24:39 +0200

    deployment fixed index.js

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,2 @@
-import axios from 'axios';
-import PlanList from '../components/PlanList';
-
-const HomePage = ({ plans }) => {
-    return (
-        <div>
-            <h1>Travel Plans</h1>
-            <PlanList plans={plans} />
-        </div>
-    );
-}
-
-export const getServerSideProps = async () => {
-    const res = await axios.get('http://localhost:3000/api/getAllPlans');
-    const plans = res.data;
-
-    return {
-        props: {
-            plans
-        },
-    }
-}
-
+const HomePage = ({  }) => {}
 export default HomePage;
