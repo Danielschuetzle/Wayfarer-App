e127451b41049c004ffa21f3a3bd342fe0af806c
Daniel Schützle <danielschuetzle@gmail.com>
2023-06-21 11:51:12 +0200

    index pages updated

--- components/Travelplan/index.js
+++ components/Travelplan/index.js
@@ -1,15 +1,18 @@
-import TravelPlanForm from '../components/TravelPlanForm';
-import TravelPlan from '../components/TravelPlan';
+import Link from 'next/link';
 
-const HomePage = ({travelPlans, onAddTravelPlan}) => {
-    return (
-    <>
-        <TravelPlanForm addTravelPlan={onAddTravelPlan} />
-        {travelPlans.map((plan, index) => (
-            <TravelPlan key={index} plan={plan} />
-        ))}
-    </>
-    )
-}
+const TravelPlan = ({ plan }) => {
+  return (
+    <div>
+      <Link href={`/plans/${plan.id}`}>
+        <a>
+          <h2>{plan.name}</h2>
+          <p>Start Date: {plan.startDate}</p>
+          <p>End Date: {plan.endDate}</p>
+          <p>Activity: {plan.activity}</p>
+        </a>
+      </Link>
+    </div>
+  );
+};
 
-export default HomePage;
+export default TravelPlan;
--- pages/index.js
+++ pages/index.js
@@ -1,2 +1,15 @@
-const HomePage = ({  }) => {}
+import TravelPlanForm from '../components/TravelPlanForm';
+import TravelPlan from '../components/TravelPlan';
+
+const HomePage = ({travelPlans, onAddTravelPlan}) => {
+    return (
+    <>
+        <TravelPlanForm addTravelPlan={onAddTravelPlan} />
+        {travelPlans.map((plan, index) => (
+            <TravelPlan key={index} plan={plan} />
+        ))}
+    </>
+    )
+}
+
 export default HomePage;
