003c618d6014acb5d5f480eceddbb864aec5da06
Daniel Schützle <danielschuetzle@gmail.com>
2023-06-21 15:29:41 +0200

    Travelplan component refactored

--- components/Travelplan/index.js
+++ components/Travelplan/index.js
@@ -1,13 +1,12 @@
-const TravelPlan = ({ plan }) => {
-    return (
-      <div>
-        <h2>{plan.name}</h2>
-        <p>Start Date: {plan.startDate}</p>
-        <p>End Date: {plan.endDate}</p>
-        <p>Activity: {plan.activity}</p>
-      </div>
-    );
-  };
-  
-  export default TravelPlan;
-  
+import React from "react";
+
+const TravelPlan = ({ plan: { name, startDate, endDate, activity } }) => (
+    <div>
+        <h2>{name}</h2>
+        <p>Start Date: {startDate}</p>
+        <p>End Date: {endDate}</p>
+        <p>Activity: {activity}</p>
+    </div>
+);
+
+export default TravelPlan;
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,17 @@
+import React from "react";
 import TravelPlanForm from "../components/TravelPlanForm";
 import TravelPlan from "../components/TravelPlan"; 
 
-
 const HomePage = ({travelPlans, onAddTravelPlan}) => {
+    const travelPlanItems = travelPlans.map((plan, index) => 
+        <TravelPlan key={index} plan={plan} />
+    );
+
     return (
-    <>
-        <TravelPlanForm addTravelPlan={onAddTravelPlan} />
-        {travelPlans.map((plan, index) => (
-            <TravelPlan key={index} plan={plan} />
-        ))}
-    </>
+        <>
+            <TravelPlanForm addTravelPlan={onAddTravelPlan} />
+            {travelPlanItems}
+        </>
     )
 }
 
