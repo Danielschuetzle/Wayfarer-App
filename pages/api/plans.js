e29c3cc75e842a88f98dae46f35f51f9d4b1f12a
Daniel Schützle <danielschuetzle@gmail.com>
2023-06-22 15:36:26 +0200

    refactoring for vercel

--- pages/api/plans.js
+++ pages/api/plans.js
@@ -1,19 +1,30 @@
-import connectToDatabase from '../../lib/connectDb';
-import { ObjectId } from 'mongodb'; // Import ObjectId from mongodb
+import { MongoClient, ObjectId } from 'mongodb';
+
+const client = new MongoClient(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 export default async function handler(req, res) {
-  const { db } = await connectToDatabase();
+  try {
+    await client.connect();
 
-  if (req.method === 'GET') {
-    const { id } = req.query;
+    const db = client.db(process.env.MONGODB_DB);
+    
+    if (req.method === 'GET') {
+      const { id } = req.query;
 
-    // If id is provided, find the specific plan. Otherwise, return all plans.
-    if (id) {
-      const plan = await db.collection("plans").findOne({ _id: new ObjectId(id) });
-      return res.json(plan);
-    } else {
-      const plans = await db.collection("plans").find({}).toArray();
-      return res.json(plans);
+      // If id is provided, find the specific plan. Otherwise, return all plans.
+      if (id) {
+        const plan = await db.collection("plans").findOne({ _id: new ObjectId(id) });
+        return res.json(plan);
+      } else {
+        const plans = await db.collection("plans").find({}).toArray();
+        return res.json(plans);
+      }
     }
+  } finally {
+    // Ensuring that the client will close when you finish/error
+    await client.close();
   }
 }
