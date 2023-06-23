ca0002be227e3788106ffcddb169992522cf48de
Daniel Schützle <danielschuetzle@gmail.com>
2023-06-23 11:05:31 +0200

    moving the database logic to the travelPlanDb.js

--- pages/api/plans.js
+++ pages/api/plans.js
@@ -1,38 +1,17 @@
-import { MongoClient, ObjectId } from 'mongodb';
-
-const client = new MongoClient(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+import { getPlans, createPlan } from '../../lib/travelPlanDb';
 
 export default async function handler(req, res) {
   try {
-    await client.connect();
-
-    const db = client.db(process.env.MONGODB_DB);
-
     if (req.method === 'GET') {
       const { id } = req.query;
-
-      // If id is provided, find the specific plan. Otherwise, return all plans.
-      if (id) {
-        const plan = await db.collection("plans").findOne({ _id: new ObjectId(id) });
-        return res.json(plan);
-      } else {
-        const plans = await db.collection("plans").find({}).toArray();
-        return res.json(plans);
-      }
+      const plans = await getPlans(id);
+      return res.json(plans);
     } else if (req.method === 'POST') {
       const plan = req.body;
-
-      // Insert the plan into the 'plans' collection
-      const result = await db.collection("plans").insertOne(plan);
-      const savedPlan = result.ops[0];
-
+      const savedPlan = await createPlan(plan);
       return res.status(201).json(savedPlan);
     }
-  } finally {
-    // Ensuring that the client will close when you finish/error
-    await client.close();
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 }
