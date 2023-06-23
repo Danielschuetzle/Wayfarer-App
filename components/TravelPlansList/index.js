90446fbd702662ebf953041dc4b6600c4a88420c
Daniel Schützle <danielschuetzle@gmail.com>
2023-06-23 16:30:19 +0200

    Planslist updated

--- components/TravelPlansList/index.js
+++ components/TravelPlansList/index.js
@@ -8,7 +8,7 @@ const fetcher = async (url) => {
 
 const TravelPlansList = () => {
   const { data: plans, error } = useSWR('/api/plans', fetcher);
-  console.log(plans);
+
   if (error) {
     return <div>Error: {error.message}</div>;
   }
@@ -25,7 +25,7 @@ const TravelPlansList = () => {
           <h2>{plan.name}</h2>
           <p>Start Date: {plan.startDate}</p>
           <p>End Date: {plan.endDate}</p>
-          <p>Activities: {plan.activities ? plan.activities.join(', ') : ''}</p>
+          {plan.activities && <p>Activities: {plan.activities.join(', ')}</p>}
         </div>
       ))}
     </div>
