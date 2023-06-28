80ce24e8e7fa8cca429fc08869a3f7b952f9925e
Daniel Schützle <danielschuetzle@gmail.com>
2023-06-28 17:08:08 +0200

    example imgs added

--- data/exampleTravelPlans.js
+++ data/exampleTravelPlans.js
@@ -6,6 +6,7 @@ const exampleTravelPlans = [
     endDate: '2023-06-15',
     activity: 'Relaxation, Swimming, Water Sports',
     tag: 'Beach',
+    picture: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1473&q=80',
   },
   {
     id: 3,
@@ -14,6 +15,7 @@ const exampleTravelPlans = [
     endDate: '2023-06-25',
     activity: 'Hiking, Camping, Nature Exploration',
     tag: 'Mountain',
+    picture: 'https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
   },
 ];
 
