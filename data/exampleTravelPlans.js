941325ddfc50ac10b93a4e83a037a5439f83b666
Daniel Schützle <danielschuetzle@gmail.com>
2023-06-29 09:57:45 +0200

    more example added

--- data/exampleTravelPlans.js
+++ data/exampleTravelPlans.js
@@ -1,5 +1,15 @@
 const exampleTravelPlans = [
   {
+    id: 1,
+    planName: 'City Exploration',
+    startDate: '2023-06-01',
+    endDate: '2023-06-05',
+    activity: 'Sightseeing, Shopping, Local Cuisine',
+    tag: 'City',
+    picture: 'https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1888&q=80',
+    budget: 500,
+  },
+  {
     id: 2,
     planName: 'Beach Vacation',
     startDate: '2023-06-11',
@@ -7,7 +17,7 @@ const exampleTravelPlans = [
     activity: 'Relaxation, Swimming, Water Sports',
     tag: 'Beach',
     picture: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1473&q=80',
-    budget: 500,
+    budget: 800,
   },
   {
     id: 3,
@@ -17,7 +27,7 @@ const exampleTravelPlans = [
     activity: 'Hiking, Camping, Nature Exploration',
     tag: 'Mountain',
     picture: 'https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
-    budget: 300,
+    budget: 400,
   },
 ];
 
